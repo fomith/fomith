235ab786e02b0ae64aa94f92d95d75d020831af9
DmitryFomenko <shaiyaby@yandex.by>
2019-11-07 13:13:03 +0300

    7.11-2-work

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 // video
 
 window.onload = function () {
-    if (window.innerWidth >= 1024)
+    if (window.innerWidth >= 1171)
         document.getElementById('video-container').innerHTML = '<video muted autoplay="autoplay" poster="./video/move.jpg" loop="loop"> <source src="./video/move.mp4" type="video/mp4" /> <source src="./video/move.webm" type="video/webm" /> <source src="./video/move.ogv" type="video/ogg" /> </video>'
     //  if (window.innerWidth <= 960)
     //  $(".services-articles").addClass("slider");
@@ -82,5 +82,88 @@ $(window).scroll(function () {
     } else {
         $('header').removeClass('header-fixed');
         $('nav_icon').removeClass('header-fixed');
+    }
+});
+
+// OPEN FULL INFO BLOCK
+
+let teamLeftTrigger = 0;
+let teamCenterLeftTrigger = 0;
+let teamCenterRightTrigger = 0;
+let teamRightTrigger = 0;
+
+$(".team-left").click(function () {
+    if (teamLeftTrigger == 0) {
+        teamLeftTrigger = 1;
+        $(".info-left").addClass("open-info-activ");
+        $(".block-one").addClass("open-info-activ");
+
+        $(".info-center-left").removeClass("open-info-activ");
+        $(".info-center-right").removeClass("open-info-activ");
+        $(".info-right").removeClass("open-info-activ");
+
+        $(".block-two").removeClass("open-info-activ");
+        $(".block-tree").removeClass("open-info-activ");
+        $(".block-four").removeClass("open-info-activ");
+        teamCenterLeftTrigger = 0;
+        teamCenterRightTrigger = 0;
+        teamRightTrigger = 0;
+    }
+});
+
+$(".team-center-left").click(function () {
+    if (teamCenterLeftTrigger == 0) {
+        teamCenterLeftTrigger = 1;
+        $(".info-center-left").addClass("open-info-activ");
+        $(".block-two").addClass("open-info-activ");
+
+        $(".info-left").removeClass("open-info-activ");
+        $(".info-center-right").removeClass("open-info-activ");
+        $(".info-right").removeClass("open-info-activ");
+
+        $(".block-one").removeClass("open-info-activ");
+        $(".block-tree").removeClass("open-info-activ");
+        $(".block-four").removeClass("open-info-activ");
+        teamLeftTrigger = 0;
+        teamCenterRightTrigger = 0;
+        teamRightTrigger = 0;
+    }
+});
+    
+$(".team-center-right").click(function () {
+    if (teamCenterRightTrigger == 0) {
+        teamCenterRightTrigger = 1;
+        $(".info-center-right").addClass("open-info-activ");
+        $(".block-tree").addClass("open-info-activ");
+
+        $(".info-left").removeClass("open-info-activ");
+        $(".info-center-left").removeClass("open-info-activ");
+        $(".info-right").removeClass("open-info-activ");
+
+        $(".block-one").removeClass("open-info-activ");
+        $(".block-two").removeClass("open-info-activ");
+        $(".block-four").removeClass("open-info-activ");
+        teamLeftTrigger = 0;
+        teamCenterLeftTrigger = 0;
+        teamRightTrigger = 0;
+    }
+});
+
+$(".team-right").click(function () {
+    if (teamRightTrigger == 0) {
+        teamRightTrigger = 1;
+        $(".info-right").addClass("open-info-activ");
+        $(".block-four").addClass("open-info-activ");
+
+        $(".info-left").removeClass("open-info-activ");
+        $(".info-center-left").removeClass("open-info-activ");
+        $(".info-center-right").removeClass("open-info-activ");
+
+        $(".block-one").removeClass("open-info-activ");
+        $(".block-two").removeClass("open-info-activ");
+        $(".block-tree").removeClass("open-info-activ");
+        teamLeftTrigger = 0;
+        teamCenterLeftTrigger = 0;
+        teamCenterRightTrigger = 0;
     }
 });
